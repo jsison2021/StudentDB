3a5b473c5da0b22e89fe6fe68bfe8d14cfaa45eb
Justin Sison <jsison2021@fau.edu>
2023-08-17 17:27:28 -0400

    Add project files.

---
+++ StudentDB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StudentDB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
